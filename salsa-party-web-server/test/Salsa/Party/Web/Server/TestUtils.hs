f88cf20f6f0805ab1f174d3eac565887677bc2c2
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-08 18:54:24 +0200

    simplified the test utils

--- salsa-party-web-server/test/Salsa/Party/Web/Server/TestUtils.hs
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/TestUtils.hs
@@ -2,40 +2,35 @@
 
 module Salsa.Party.Web.Server.TestUtils where
 
-import Control.Monad
-import Control.Monad.IO.Class
 import Control.Monad.Logger
-import Database.Persist.Sqlite
+import Network.HTTP.Client as HTTP
 import Path.IO
 import Salsa.Party.Web.Server.Application ()
 import Salsa.Party.Web.Server.DB
 import Salsa.Party.Web.Server.Foundation
 import Salsa.Party.Web.Server.Static
 import Test.Syd
+import Test.Syd.Path
+import Test.Syd.Persistent.Sqlite
+import Test.Syd.Wai
 import Test.Syd.Yesod
 
 type ServerSpec = YesodSpec App
 
 serverSpec :: ServerSpec -> Spec
-serverSpec =
-  around
-    ( \func -> runNoLoggingT $
-        withSqlitePool ":memory:" 1 $ \pool -> do
-          runSqlPool (void $ runMigrationQuiet migrateAll) pool
-          liftIO $
-            withSystemTempDir "salsa" $ \tdir ->
-              func (pool, tdir)
-    )
-    . yesodSpecWithSiteGeneratorAndArgument
-      ( \(pool, tdir) -> do
-          sessionKeyFile <- resolveFile tdir "session-key.aes"
-          pure
-            App
-              { appLogLevel = LevelWarn,
-                appStatic = salsaPartyWebServerStatic,
-                appConnectionPool = pool,
-                appSessionKeyFile = sessionKeyFile,
-                appGoogleAnalyticsTracking = Nothing,
-                appGoogleSearchConsoleVerification = Nothing
-              }
-      )
+serverSpec = managerSpec . yesodSpecWithSiteSetupFunc serverSetupFunc
+
+serverSetupFunc :: HTTP.Manager -> SetupFunc App
+serverSetupFunc _ = do
+  tdir <- tempDirSetupFunc "salsa"
+  pool <- connectionPoolSetupFunc migrateAll
+  sessionKeyFile <- resolveFile tdir "session-key.aes"
+  pure
+    App
+      { appLogLevel = LevelWarn,
+        appStatic = salsaPartyWebServerStatic,
+        appConnectionPool = pool,
+        appSessionKeyFile = sessionKeyFile,
+        appGoogleAnalyticsTracking = Nothing,
+        appGoogleSearchConsoleVerification = Nothing
+      }
