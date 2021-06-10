db32e627abed47d5fa81ddd927b5512093d55148
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-10 19:26:21 +0200

    refactored out a testutil

--- salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/PartySpec.hs
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/PartySpec.hs
@@ -1,22 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 
 module Salsa.Party.Web.Server.Handler.PartySpec (spec) where
 
-import Control.Monad.Reader
-import Data.Fixed
-import qualified Data.Text as T
-import Data.Time
-import qualified Database.Persist as DB
-import Database.Persist.Sql (SqlPersistT, runSqlPool)
 import Salsa.Party.Web.Server.Handler.Party
 import Salsa.Party.Web.Server.Handler.TestImport
-
-data Location = Location
-  { locationLat :: !Nano,
-    locationLon :: !Nano
-  }
-  deriving (Show, Eq)
 
 spec :: Spec
 spec = serverSpec $ do
@@ -57,31 +44,3 @@ spec = serverSpec $ do
     yit "GETs a 404 for a nonexistent party" $ do
       get $ PartyR $ toSqlKey 666
       statusIs 404
-
-submitParty :: PartyForm -> Location -> YesodClientM App ()
-submitParty PartyForm {..} Location {..} = do
-  -- Put the address in the database already so we don't need to use an external service for geocoding
-  testDB $ DB.insert_ $ Place {placeLat = locationLat, placeLon = locationLon, placeQuery = partyFormAddress}
-  get SubmitPartyR
-  statusIs 200
-  request $ do
-    setMethod methodPost
-    setUrl SubmitPartyR
-    addToken
-    addPostParam "title" partyFormTitle
-    addPostParam "day" $ T.pack $ formatTime defaultTimeLocale "%F" partyFormDay
-    addPostParam "address" partyFormAddress
-    forM_ partyFormDescription $ \description -> addPostParam "description" $ unTextarea description
-    forM_ partyFormStart $ \start -> addPostParam "start" $ T.pack $ formatTime defaultTimeLocale "%H:%M:%S" start
-  statusIs 303
-  errOrLoc <- getLocation
-  case errOrLoc of
-    Left err -> liftIO $ expectationFailure $ T.unpack err
-    Right loc -> case loc of
-      PartyR _ -> pure ()
-      _ -> liftIO $ expectationFailure "Location should have been PartyR"
-
-testDB :: SqlPersistT IO a -> YesodClientM App a
-testDB func = do
-  pool <- asks $ appConnectionPool . yesodClientSite
-  liftIO $ runSqlPool func pool
--- salsa-party-web-server/test/Salsa/Party/Web/Server/TestUtils.hs
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/TestUtils.hs
@@ -1,19 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Salsa.Party.Web.Server.TestUtils where
 
 import Control.Monad.Logger
+import Control.Monad.Reader
+import Data.Fixed
+import qualified Data.Text as T
+import Data.Time
+import qualified Database.Persist as DB
+import qualified Database.Persist.Sql as DB
 import Network.HTTP.Client as HTTP
 import Path.IO
 import Salsa.Party.Web.Server.Application ()
 import Salsa.Party.Web.Server.DB
 import Salsa.Party.Web.Server.Foundation
+import Salsa.Party.Web.Server.Handler.Party
 import Salsa.Party.Web.Server.Static
 import Test.Syd
 import Test.Syd.Path
 import Test.Syd.Persistent.Sqlite
-import Test.Syd.Wai
+import Test.Syd.Wai (managerSpec)
 import Test.Syd.Yesod
+import Yesod (Textarea (..))
 
 type ServerSpec = YesodSpec App
 
@@ -35,3 +44,37 @@ serverSetupFunc man = do
         appGoogleAnalyticsTracking = Nothing,
         appGoogleSearchConsoleVerification = Nothing
       }
+
+data Location = Location
+  { locationLat :: !Nano,
+    locationLon :: !Nano
+  }
+  deriving (Show, Eq)
+
+submitParty :: PartyForm -> Location -> YesodClientM App ()
+submitParty PartyForm {..} Location {..} = do
+  -- Put the address in the database already so we don't need to use an external service for geocoding
+  testDB $ DB.insert_ $ Place {placeLat = locationLat, placeLon = locationLon, placeQuery = partyFormAddress}
+  get SubmitPartyR
+  statusIs 200
+  request $ do
+    setMethod methodPost
+    setUrl SubmitPartyR
+    addToken
+    addPostParam "title" partyFormTitle
+    addPostParam "day" $ T.pack $ formatTime defaultTimeLocale "%F" partyFormDay
+    addPostParam "address" partyFormAddress
+    forM_ partyFormDescription $ \description -> addPostParam "description" $ unTextarea description
+    forM_ partyFormStart $ \start -> addPostParam "start" $ T.pack $ formatTime defaultTimeLocale "%H:%M:%S" start
+  statusIs 303
+  errOrLoc <- getLocation
+  case errOrLoc of
+    Left err -> liftIO $ expectationFailure $ T.unpack err
+    Right loc -> case loc of
+      PartyR _ -> pure ()
+      _ -> liftIO $ expectationFailure "Location should have been PartyR"
+
+testDB :: DB.SqlPersistT IO a -> YesodClientM App a
+testDB func = do
+  pool <- asks $ appConnectionPool . yesodClientSite
+  liftIO $ runSqlPool func pool
