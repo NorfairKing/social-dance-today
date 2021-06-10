ae1e43c43b8ff4c9a555ba9097bf238b5fc11193
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-10 19:13:09 +0200

    test for getting a party page

--- salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/PartySpec.hs
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/PartySpec.hs
@@ -24,8 +24,21 @@ spec = serverSpec $ do
     yit "GETs a 200 for SubmitPartyR" $ do
       get SubmitPartyR
       statusIs 200
-    yit "Can create a party by POSTing to SubmitPartyR" $ do
+    yit "Can create a party by POSTing to SubmitPartyR" $
       submitParty
+        PartyForm
+          { partyFormTitle = "example title",
+            partyFormDay = fromGregorian 2021 06 08,
+            partyFormAddress = "Badenerstrasse 551, 8048 Zürich",
+            partyFormDescription = Just "Example description",
+            partyFormStart = Just $ TimeOfDay 21 00 00
+          }
+        Location -- TODO make this location, but not the party (?), generated
+          { locationLat = 0,
+            locationLon = 0
+          }
+    yit "Can get the party page for an existing party" $ do
+      submitParty -- TODO make this party generated
         PartyForm
           { partyFormTitle = "example title",
             partyFormDay = fromGregorian 2021 06 08,
