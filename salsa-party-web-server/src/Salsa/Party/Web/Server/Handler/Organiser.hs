5c4b4bf242756023243bff458b8fa88adfea2fb7
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-14 10:58:13 +0200

    overview of parties for an organiser

--- salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Organiser.hs
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Organiser.hs
@@ -6,6 +6,7 @@
 module Salsa.Party.Web.Server.Handler.Organiser where
 
 import qualified Data.Text as T
+import qualified Database.Esqueleto as E
 import Salsa.Party.Web.Server.Handler.Import
 
 data OrganiserForm = OrganiserForm
@@ -57,4 +58,14 @@ organiserFormPage mResult = do
 getOrganiserR :: OrganiserId -> Handler Html
 getOrganiserR organiserId = do
   Organiser {..} <- runDB $ get404 organiserId
+  today <- liftIO $ utctDay <$> getCurrentTime
+  parties <- runDB $
+    E.select $
+      E.from $ \(party `E.InnerJoin` p `E.LeftOuterJoin` mPoster) -> do
+        E.on (party E.^. PartyPlace E.==. p E.^. PlaceId)
+        E.on (E.just (party E.^. PartyId) E.==. mPoster E.?. PosterParty)
+        E.where_ (party E.^. PartyOrganiser E.==. E.val organiserId)
+        E.where_ (party E.^. PartyDay E.>=. E.val today)
+        E.orderBy [E.asc $ party E.^. PartyDay]
+        pure (party, p, mPoster E.?. PosterId)
   withNavBar $(widgetFile "organiser")
