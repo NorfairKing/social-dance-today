3e672006407b997a2f20d8629174b5899fe81bbf
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-16 23:02:51 +0200

    facebook open graph data

--- salsa-party-web-server/src/Salsa/Party/Web/Server/Migration.hs
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Migration.hs
@@ -5,17 +5,21 @@ module Salsa.Party.Web.Server.Migration where
 
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Monad.Logger
 import Database.Persist.Sql
 import Salsa.Party.Web.Server.DB
 
-completeServerMigration :: MonadIO m => Bool -> SqlPersistT m ()
+completeServerMigration :: (MonadIO m, MonadLogger m) => Bool -> SqlPersistT m ()
 completeServerMigration quiet = do
+  logInfoN "Running automatic migrations"
   (if quiet then void . runMigrationQuiet else runMigration) automaticMigrations
+  logInfoN "Setting up standard places in database"
   forM_ locations $ \location@Place {..} -> do
     mPlace <- getBy (UniquePlaceQuery placeQuery)
     case mPlace of
       Just _ -> pure ()
       Nothing -> insert_ location
+  logInfoN "Migrations done."
 
 locations :: [Place]
 locations =
