7526b03d48f65a2d667426e651daa341bd479081
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-06-24 21:30:33 +0200

    refactor the search query into its own module

---
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Search/Query.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Salsa.Party.Web.Server.Handler.Search.Query where
+
+import Control.Monad
+import qualified Database.Esqueleto as E
+import Salsa.Party.Web.Server.Distance
+import Salsa.Party.Web.Server.Handler.Import
+
+-- For a given day and a given place,
+-- find all parties sorted by distance.
+searchQuery :: MonadIO m => Day -> Coordinates -> SqlPersistT m [(Entity Party, Entity Place, Maybe CASKey)]
+searchQuery day coordinates@Coordinates {..} = do
+  rawResults <- E.select $
+    E.from $ \(party `E.InnerJoin` p `E.LeftOuterJoin` mPoster) -> do
+      E.on (party E.^. PartyPlace E.==. p E.^. PlaceId)
+      E.on (E.just (party E.^. PartyId) E.==. mPoster E.?. PosterParty)
+      E.where_ (party E.^. PartyDay E.==. E.val day)
+      -- We want a very rough filter of parties by distance.
+      -- What follows here is a rough estimate
+      E.where_ $
+        E.between
+          (p E.^. PlaceLat)
+          (E.val (coordinatesLat - roughMaxLatDistance), E.val (coordinatesLat + roughMaxLatDistance))
+      E.where_ $
+        E.between
+          (p E.^. PlaceLon)
+          (E.val (coordinatesLon - roughMaxLonDistance), E.val (coordinatesLon + roughMaxLonDistance))
+
+      let latDiff = p E.^. PlaceLat E.-. E.val coordinatesLat
+      let lonDiff = p E.^. PlaceLon E.-. E.val coordinatesLon
+      let latDiffSquared = latDiff E.*. latDiff
+      let lonDiffSquared = lonDiff E.*. lonDiff
+      -- Luckily the square function is monotone so we don't need to sqrt here
+      let distSquared = latDiffSquared E.+. lonDiffSquared
+      E.orderBy [E.asc distSquared]
+      pure (party, p, mPoster E.?. PosterKey)
+
+  pure $ postProcess coordinates rawResults
+
+-- One degree longitude is 111km
+roughMaxLatDistance :: Nano
+roughMaxLatDistance = realToFrac maximumDistance / 111_000
+
+-- Five degrees longitude is 555km at the equator and about 100km in north svalbard
+roughMaxLonDistance :: Nano
+roughMaxLonDistance = 5 * realToFrac maximumDistance / 111_000
+
+postProcess ::
+  Coordinates ->
+  [(Entity Party, Entity Place, E.Value (Maybe CASKey))] ->
+  [(Entity Party, Entity Place, Maybe CASKey)]
+postProcess coordinates = mapMaybe $ \(party, place, E.Value mCasKey) -> do
+  guard $
+    coordinates `distanceTo` placeCoordinates (entityVal place)
+      <= maximumDistance
+  pure (party, place, mCasKey)
+
+maximumDistance :: Double
+maximumDistance = 100_000 -- 100 km
---
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/Search/QuerySpec.hs
@@ -0,0 +1,97 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
+
+module Salsa.Party.Web.Server.Handler.Search.QuerySpec (spec) where
+
+import qualified Database.Persist as DB
+import Salsa.Party.Web.Server.Handler.Search.Query
+import Salsa.Party.Web.Server.Handler.TestImport
+
+spec :: Spec
+spec = do
+  dbSpec $ do
+    describe "searchQuery" $ do
+      it "runs without results" $ \pool ->
+        forAllValid $ \day ->
+          forAllValid $ \place ->
+            flip runSqlPool pool $ do
+              ps <- searchQuery @IO day place
+              liftIO $ ps `shouldBe` []
+      it "runs correctly with these three parties where one is on a different day" $ \pool ->
+        forAllValid $ \party1Prototype ->
+          forAllValid $ \party2Prototype ->
+            forAllValid $ \party3Prototype ->
+              forAllValid $ \day ->
+                flip runSqlPool pool $ do
+                  let queryPlace = Place {placeQuery = "Search Place", placeLat = 0, placeLon = 0}
+                  _ <- DB.insert queryPlace
+                  let place1 = Place {placeQuery = "Place 1", placeLat = 0, placeLon = 0.05}
+                  place1Id <- DB.insert place1
+                  let place2 = Place {placeQuery = "Place 2", placeLat = 0, placeLon = 0.1}
+                  place2Id <- DB.insert place2
+                  let place3 = Place {placeQuery = "Place 3", placeLat = 0, placeLon = 0.15}
+                  place3Id <- DB.insert place3
+                  let party1 =
+                        party1Prototype
+                          { partyDay = day,
+                            partyPlace = place1Id
+                          }
+                  party1Id <- DB.insert party1
+                  let party2 =
+                        party2Prototype
+                          { partyDay = day,
+                            partyPlace = place2Id
+                          }
+                  party2Id <- DB.insert party2
+                  -- close to party 1, but the next day
+                  let party3 =
+                        party3Prototype
+                          { partyDay = addDays 1 day,
+                            partyPlace = place3Id
+                          }
+                  _ <- DB.insert party3
+                  ps <- searchQuery @IO day (placeCoordinates queryPlace)
+                  liftIO $
+                    ps
+                      `shouldBe` [ (Entity party1Id party1, Entity place1Id place1, Nothing),
+                                   (Entity party2Id party2, Entity place2Id place2, Nothing)
+                                 ]
+      it "runs correctly with these three parties where one is too far away" $ \pool ->
+        forAllValid $ \party1Prototype ->
+          forAllValid $ \party2Prototype ->
+            forAllValid $ \party3Prototype ->
+              forAllValid $ \day ->
+                flip runSqlPool pool $ do
+                  let queryPlace = Place {placeQuery = "Search Place", placeLat = 0, placeLon = 0}
+                  _ <- DB.insert queryPlace
+                  let place1 = Place {placeQuery = "Place 1", placeLat = 0, placeLon = 0.1}
+                  place1Id <- DB.insert place1
+                  let place2 = Place {placeQuery = "Place 2", placeLat = 0.1, placeLon = 0}
+                  place2Id <- DB.insert place2
+                  let place3 = Place {placeQuery = "Place 3", placeLat = 5, placeLon = 5}
+                  place3Id <- DB.insert place3
+                  let party1 =
+                        party1Prototype
+                          { partyDay = day,
+                            partyPlace = place1Id
+                          }
+                  party1Id <- DB.insert party1
+                  let party2 =
+                        party2Prototype
+                          { partyDay = day,
+                            partyPlace = place2Id
+                          }
+                  party2Id <- DB.insert party2
+                  -- close to party 1, but the next day
+                  let party3 =
+                        party3Prototype
+                          { partyDay = day,
+                            partyPlace = place3Id
+                          }
+                  _ <- DB.insert party3
+                  ps <- searchQuery @IO day (placeCoordinates queryPlace)
+                  liftIO $
+                    ps
+                      `shouldBe` [ (Entity party1Id party1, Entity place1Id place1, Nothing),
+                                   (Entity party2Id party2, Entity place2Id place2, Nothing)
+                                 ]
