de78054d60458220aa9668ca28126f4018beb000
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-07-07 23:43:17 +0200

    more tests for query

--- salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/SearchSpec.hs
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/SearchSpec.hs
@@ -8,6 +8,8 @@ import Salsa.Party.Web.Server.Handler.TestImport
 spec :: Spec
 spec = do
   serverSpec $ do
+    -- These tests are just for the form handling.
+    -- They don't test search because there won't be results.
     describe "QueryR" $ do
       it "Can GET a 400 query page for an empty query" $ do
         post QueryR
@@ -15,14 +17,55 @@ spec = do
 
       it "Can GET a 200 query page for a nonempty query" $ \yc ->
         forAll (genValid `suchThat` (not . T.null)) $ \query ->
-          forAllValid $ \location ->
+          forAllValid $ \coordinates ->
             runYesodClientM yc $ do
-              testDB $ insertPlace query location
+              testDB $ insertPlace query coordinates
               request $ do
                 setMethod methodPost
                 setUrl QueryR
                 addPostParam "address" query
               statusIs 303
+              locationShouldBe $ SearchR query
+              _ <- followRedirect
+              statusIs 200
+
+      it "Can GET a 200 query page for a nonempty query and day" $ \yc ->
+        forAll (genValid `suchThat` (not . T.null)) $ \query ->
+          forAllValid $ \day ->
+            forAllValid $ \coordinates ->
+              runYesodClientM yc $ do
+                testDB $ insertPlace query coordinates
+                request $ do
+                  setMethod methodPost
+                  setUrl QueryR
+                  addPostParam "address" query
+                  addPostParam "day" $ T.pack $ show (day :: Day)
+                statusIs 303
+                locationShouldBe $ SearchR query
+                _ <- followRedirect
+                statusIs 200
+
+      it "Can GET a 200 query page by coordinates" $ \yc ->
+        forAllValid $ \coordinates ->
+          runYesodClientM yc $ do
+            request $ do
+              setMethod methodPost
+              setUrl QueryR
+              addPostParam "latitude" $ T.pack $ show $ coordinatesLat coordinates
+              addPostParam "longitude" $ T.pack $ show $ coordinatesLon coordinates
+            statusIs 200
+
+      it "Can GET a 200 query page by coordinates and day" $ \yc ->
+        forAllValid $ \coordinates ->
+          forAllValid $ \day ->
+            runYesodClientM yc $ do
+              request $ do
+                setMethod methodPost
+                setUrl QueryR
+                addPostParam "latitude" $ T.pack $ show $ coordinatesLat coordinates
+                addPostParam "longitude" $ T.pack $ show $ coordinatesLon coordinates
+                addPostParam "day" $ T.pack $ show (day :: Day)
+              statusIs 200
 
     describe "SearchR" $ do
       it "Can GET a 200 place page for a place" $ \yc ->
