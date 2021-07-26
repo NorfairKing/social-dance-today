ae20368528ad37291fbbe454e3374f54d6f77761
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-07-26 22:25:59 +0200

    users and organisers charts

--- salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Admin/Charts.hs
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Admin/Charts.hs
@@ -22,24 +22,39 @@ import Text.Julius
 getAdminChartsR :: Handler Html
 getAdminChartsR = do
   today <- liftIO $ utctDay <$> getCurrentTime
-  acqExternalEventsSource <- runDB $ selectSourceRes [] [Asc ExternalEventCreated]
+  acqExternalEventsSource <- runDB $ selectSourceRes [] [Asc ExternalEventId]
   dayCountMapOfExternalEvents <- makeUpcomingEventsMap today acqExternalEventsSource $ \(Entity _ ExternalEvent {..}) ->
     (utctDay externalEventCreated, externalEventDay)
-  acqPartiesSource <- runDB $ selectSourceRes [] [Asc PartyCreated]
+
+  acqPartiesSource <- runDB $ selectSourceRes [] [Asc PartyId]
   dayCountMapOfParties <- makeUpcomingEventsMap today acqPartiesSource $ \(Entity _ Party {..}) ->
     (utctDay partyCreated, partyDay)
-  let dayCountMap = combineUpcomingEventsMap dayCountMapOfParties dayCountMapOfExternalEvents
 
-  let minDay = case (M.lookupMin dayCountMapOfExternalEvents, M.lookupMin dayCountMapOfParties) of
-        (Nothing, Nothing) -> today
-        (Just (d, _), Nothing) -> d
-        (Nothing, Just (d, _)) -> d
-        (Just (d1, _), Just (d2, _)) -> min d1 d2
+  let dayCountMapOfTotal = combineUpcomingEventsMap dayCountMapOfParties dayCountMapOfExternalEvents
+
+  acqUsersSource <- runDB $ selectSourceRes [] [Asc UserId]
+  dayCountOfUsers <- makeDayCountMap today acqUsersSource $ utctDay . userCreated . entityVal
+  acqOrganisersSource <- runDB $ selectSourceRes [] [Asc OrganiserId]
+  dayCountOfOrganisers <- makeDayCountMap today acqOrganisersSource $ utctDay . organiserCreated . entityVal
+
+  let minDay = minimum $ map fst $ mapMaybe M.lookupMin [dayCountMapOfExternalEvents, dayCountMapOfParties]
       maxDay = today
 
   withNavBar $ do
     addScriptRemote "https://cdn.jsdelivr.net/npm/chart.js@3.4.1/dist/chart.min.js"
     $(widgetFile "admin/charts")
+
+makeDayCountMap :: MonadUnliftIO m => Day -> Acquire (ConduitT () a m ()) -> (a -> Day) -> m (Map Day Word64)
+makeDayCountMap today source func = makeDayMap source (\m a -> addToDayMap today m (func a))
+
+addToDayMap :: Day -> Map Day Word64 -> Day -> Map Day Word64
+addToDayMap today m created =
+  let days = [created .. today]
+      mapOfOnes = M.fromAscList $ map (\d -> (d, 1)) days
+   in M.unionWith (+) mapOfOnes m
+
+combineUpcomingEventsMap :: Map Day Word64 -> Map Day Word64 -> Map Day Word64
+combineUpcomingEventsMap = M.unionWith (+)
 
 makeUpcomingEventsMap :: MonadUnliftIO m => Day -> Acquire (ConduitT () a m ()) -> (a -> (Day, Day)) -> m (Map Day Word64)
 makeUpcomingEventsMap today source func =
@@ -50,21 +65,18 @@ makeUpcomingEventsMap today source func =
          in addToUpcomingEventsMap today m created scheduled
     )
 
-makeDayMap :: MonadUnliftIO m => Acquire (ConduitT () a m ()) -> (Map Day Word64 -> a -> Map Day Word64) -> m (Map Day Word64)
-makeDayMap acqSource func = withAcquire acqSource $ \source ->
-  runConduit $ source .| C.foldl func M.empty
-
-combineUpcomingEventsMap :: Map Day Word64 -> Map Day Word64 -> Map Day Word64
-combineUpcomingEventsMap = M.unionWith (+)
-
 addToUpcomingEventsMap :: Day -> Map Day Word64 -> Day -> Day -> Map Day Word64
 addToUpcomingEventsMap today m created scheduled =
   let days = [created .. min today scheduled]
       mapOfOnes = M.fromAscList $ map (\d -> (d, 1)) days
    in M.unionWith (+) mapOfOnes m
 
-lineChartOverTime :: Day -> Day -> [(Text, Map Day Word64)] -> Widget
-lineChartOverTime minDay maxDay maps = do
+makeDayMap :: MonadUnliftIO m => Acquire (ConduitT () a m ()) -> (Map Day Word64 -> a -> Map Day Word64) -> m (Map Day Word64)
+makeDayMap acqSource func = withAcquire acqSource $ \source ->
+  runConduit $ source .| C.foldl func M.empty
+
+lineChart :: Day -> Day -> [(Text, Map Day Word64)] -> Widget
+lineChart minDay maxDay maps = do
   let days = [minDay .. maxDay]
       labels = toJSON $ map (formatTime defaultTimeLocale "%F") days
       datumsFor dayCountMap = toJSON $ map (\d -> fromMaybe 0 $ M.lookup d dayCountMap) days
