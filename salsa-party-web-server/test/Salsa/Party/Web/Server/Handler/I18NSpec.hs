68894857d6999c92109b9969adf5c7473571574d
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-08-01 18:14:21 +0200

    made the i18n tests more robust

--- salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/I18NSpec.hs
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/I18NSpec.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Salsa.Party.Web.Server.Handler.I18NSpec (spec) where
 
+import qualified Data.ByteString as SB
 import Data.List
 import Data.Maybe
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 import Path
 import Path.IO
 import Salsa.Party.Web.Server.Handler.TestImport
@@ -13,26 +17,32 @@ spec =
   describe "I18N" $ do
     scenarioDir "messages" $ \fp ->
       it (fp <> " has no more TODOs") $ do
-        contents <- readFile fp
-        contents `shouldNotSatisfy` ("TODO" `isInfixOf`)
+        contents <- SB.readFile fp
+        contents `shouldNotSatisfy` ("TODO" `SB.isInfixOf`)
 
     it "Has translations for every string" $ do
       translationsDir <- resolveDir' "messages"
       mainFile <- resolveFile translationsDir "en.msg"
       allFiles <- snd <$> listDir translationsDir
       let otherFiles = filter (/= mainFile) allFiles
-      mainFileContents <- readFile $ fromAbsFile mainFile
-      let mainMessages = messagesIn mainFileContents
-      tups <- forM otherFiles $ \otherFile -> do
-        otherFileContents <- readFile $ fromAbsFile otherFile
-        let missingMessages = mainMessages \\ messagesIn otherFileContents
-        pure (filename otherFile, missingMessages)
-      when (not (all (null . snd) tups)) $
-        expectationFailure $
-          unlines $
-            flip map tups $ \(name, missingMessages) ->
+      mainFileContents <- SB.readFile $ fromAbsFile mainFile
+      case TE.decodeUtf8' mainFileContents of
+        Left _ -> expectationFailure "main file did not contain valid utf8"
+        Right mainContents -> do
+          let mainMessages = messagesIn (T.unpack mainContents)
+          tups <- forM otherFiles $ \otherFile -> do
+            otherFileContents <- SB.readFile $ fromAbsFile otherFile
+            case TE.decodeUtf8' otherFileContents of
+              Left _ -> pure (filename otherFile, [])
+              Right otherContents -> do
+                let missingMessages = mainMessages \\ messagesIn (T.unpack otherContents)
+                pure (filename otherFile, missingMessages)
+          when (not (all (null . snd) tups)) $
+            expectationFailure $
               unlines $
-                unwords ["Missing messages in", fromRelFile name] : missingMessages
+                flip map tups $ \(name, missingMessages) ->
+                  unlines $
+                    unwords ["Missing messages in", fromRelFile name] : missingMessages
 
 messagesIn :: String -> [String]
 messagesIn contents =
