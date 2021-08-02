cdf15babe637444cd5c5b246c53f84d67b57af59
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-08-02 11:16:56 +0200

    Next occurrence

--- salsa-party-web-server/src/Salsa/Party/DB/Recurrence.hs
+++ salsa-party-web-server/src/Salsa/Party/DB/Recurrence.hs
@@ -6,9 +6,11 @@ module Salsa.Party.DB.Recurrence where
 
 import Data.Aeson
 import Data.ByteString
+import Data.Fixed
 import Data.Proxy
 import qualified Data.Text as T
 import Data.Time
+import Data.Time.Calendar
 import Data.Validity
 import Data.Validity.Time
 import Database.Persist
@@ -33,7 +35,7 @@ instance ToJSON Recurrence where
   toJSON =
     object
       . \case
-        WeeklyRecurrence dayOfWeek -> [("type", "weekly"), "day" .= dayOfWeek]
+        WeeklyRecurrence dow -> [("type", "weekly"), "day" .= dow]
 
 instance PersistField Recurrence where
   fromPersistValue = fromPersistValueJSON
@@ -41,3 +43,23 @@ instance PersistField Recurrence where
 
 instance PersistFieldSql Recurrence where
   sqlType Proxy = sqlType (Proxy :: Proxy ByteString)
+
+-- For a given recurrence and the current day, calculate the next occurrence
+-- The 'current' day will be the previous occurrence, or 'today' if the occurrence has never happened yet.
+nextOccurrence :: Recurrence -> Day -> Day
+nextOccurrence = \case
+  WeeklyRecurrence dow -> nextWeeklyRecurrence dow
+
+nextWeeklyOccurrence :: DayOfWeek -> Day -> Day
+nextWeeklyOccurrence dow today = firstDayOfWeekOnAfter dow (addDays 1 today)
+
+-- What follows is in a newer time version and can be removed when we upgrade to that newer time version
+
+-- | @dayOfWeekDiff a b = a - b@ in range 0 to 6.
+-- The number of days from b to the next a.
+dayOfWeekDiff :: DayOfWeek -> DayOfWeek -> Int
+dayOfWeekDiff a b = mod' (fromEnum a - fromEnum b) 7
+
+-- | The first day-of-week on or after some day
+firstDayOfWeekOnAfter :: DayOfWeek -> Day -> Day
+firstDayOfWeekOnAfter dw d = addDays (toInteger $ dayOfWeekDiff dw $ dayOfWeek d) d
--- salsa-party-web-server/test/Salsa/Party/DB/RecurrenceSpec.hs
+++ salsa-party-web-server/test/Salsa/Party/DB/RecurrenceSpec.hs
@@ -27,3 +27,15 @@ spec = do
   persistSpecOnValid @Recurrence
   it "Outputs WeeklyRecurrence the same as before" $
     pureGoldenJSONValueFile "test_resources/recurrence/weekly.json" $ WeeklyRecurrence Friday
+  describe "nextOccurrence" $ do
+    it "is always different from the current day" $
+      forAllValid $ \recurrence ->
+        forAllValid $ \day ->
+          nextOccurrence recurrence day `shouldNotBe` day
+  describe "nextWeeklyOccurrence" $ do
+    it "works for this friday party" $
+      nextWeeklyOccurrence Friday (fromGregorian 2021 08 02) `shouldBe` fromGregorian 2021 08 06
+    it "is always no the given day" $
+      forAllValid $ \day ->
+        forAllValid $ \dow ->
+          dayOfWeek (nextWeeklyOccurrence dow day) `shouldBe` dow
