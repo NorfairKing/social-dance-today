8f771969341ee6fb5f4b1c6f25cd30d052d36f2f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-08-02 19:30:25 +0200

    Failing tests for adding and editing schedules

--- salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/Account/ScheduleSpec.hs
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/Account/ScheduleSpec.hs
@@ -69,11 +69,114 @@ spec = serverSpec $ do
               verifyScheduleAddedWithPoster scheduleUuid_ scheduleForm_ poster_
 
   describe "AccountScheduleR" $ do
-    pending "cannot GET a nonexistent schedule"
-    pending "can GET an existent schedule"
-    pending "cannot GET another organiser's schedule"
-    pending "can edit an existing schedule"
-    pending "can edit an existing schedule's poster"
-    pending "cannot edit a nonexisting schedule"
-    pending "cannot edit another nonexisting schedule"
-    pending "cannot edit another organiser's schedule"
+    it "can GET an existent schedule" $ \yc -> do
+      forAllValid $ \organiserForm_ ->
+        forAllValid $ \scheduleForm_ ->
+          forAllValid $ \location ->
+            withAnyLoggedInUser_ yc $ do
+              testSubmitOrganiser organiserForm_
+              scheduleId <-
+                testAddSchedule
+                  scheduleForm_
+                  location
+              get $ AccountR $ AccountScheduleR scheduleId
+              statusIs 200
+
+    it "cannot GET a nonexistent schedule" $ \yc -> do
+      forAllValid $ \organiserForm_ ->
+        withAnyLoggedInUser_ yc $ do
+          testSubmitOrganiser organiserForm_
+          uuid <- nextRandomUUID
+          get $ AccountR $ AccountScheduleR uuid
+          statusIs 404
+
+    it "cannot GET another organiser's schedule" $ \yc ->
+      forAllValid $ \testUser1 ->
+        forAllValid $ \testUser2 ->
+          forAllValid $ \organiser1Form_ ->
+            forAllValid $ \organiser2Form_ ->
+              forAllValid $ \scheduleForm_ ->
+                forAllValid $ \location -> runYesodClientM yc $ do
+                  scheduleId <- asNewUser testUser1 $ do
+                    testSubmitOrganiser organiser1Form_
+                    testAddSchedule scheduleForm_ location
+                  asNewUser testUser2 $ do
+                    testSubmitOrganiser organiser2Form_
+                    get $ AccountR $ AccountScheduleR scheduleId
+                    statusIs 403
+
+    it "can edit an existing schedule" $ \yc ->
+      forAllValid $ \organiserForm_ ->
+        forAllValid $ \addScheduleForm_ ->
+          forAllValid $ \editScheduleForm_ ->
+            forAllValid $ \location ->
+              withAnyLoggedInUser_ yc $ do
+                testSubmitOrganiser organiserForm_
+                scheduleUuid_ <-
+                  testAddSchedule
+                    addScheduleForm_
+                    location
+                get $ AccountR $ AccountScheduleR scheduleUuid_
+                statusIs 200
+                testEditSchedule scheduleUuid_ editScheduleForm_ location
+                statusIs 303
+                _ <- followRedirect
+                statusIs 200
+                verifyScheduleEdited scheduleUuid_ editScheduleForm_
+
+    it "can edit an existing schedule's poster" $ \yc ->
+      forAllValid $ \organiserForm_ ->
+        forAllValid $ \addScheduleForm_ ->
+          forAllValid $ \editScheduleForm_ ->
+            forAllValid $ \location -> do
+              withAnyLoggedInUser_ yc $ do
+                testSubmitOrganiser organiserForm_
+                poster1 <- readTestFile "test_resources/posters/1.png"
+                poster2 <- readTestFile "test_resources/posters/2.png"
+                scheduleUuid_ <-
+                  testAddScheduleWithPoster
+                    addScheduleForm_
+                    location
+                    poster1
+                mSchedule <- testDB $ DB.getBy $ UniqueScheduleUUID scheduleUuid_
+                mCasKey1 <- case mSchedule of
+                  Nothing -> liftIO $ expectationFailure "expected the first schedule to exist."
+                  Just (Entity scheduleId _) -> testDB $ getPosterForSchedule scheduleId
+                -- There is now a poster.
+                liftIO $ mCasKey1 `shouldBe` testFileCASKey poster1
+                get $ AccountR $ AccountScheduleR scheduleUuid_
+                statusIs 200
+                testEditScheduleWithPoster scheduleUuid_ editScheduleForm_ location poster2
+                statusIs 303
+                _ <- followRedirect
+                statusIs 200
+                verifyScheduleEditedWithPoster scheduleUuid_ editScheduleForm_ poster2
+
+    it "cannot edit a nonexisting schedule" $ \yc ->
+      forAllValid $ \organiserForm_ ->
+        forAllValid $ \scheduleForm_ ->
+          forAllValid $ \location ->
+            withAnyLoggedInUser_ yc $ do
+              testSubmitOrganiser organiserForm_
+              get $ AccountR AccountSubmitScheduleR
+              statusIs 200
+              uuid <- nextRandomUUID
+              testEditSchedule uuid scheduleForm_ location
+              statusIs 404
+
+    it "cannot edit another organiser's schedule" $ \yc ->
+      forAllValid $ \testUser1 ->
+        forAllValid $ \testUser2 ->
+          forAllValid $ \organiser1Form_ ->
+            forAllValid $ \organiser2Form_ ->
+              forAllValid $ \addScheduleForm_ ->
+                forAllValid $ \editScheduleForm_ ->
+                  forAllValid $ \location -> runYesodClientM yc $ do
+                    scheduleUuid_ <- asNewUser testUser1 $ do
+                      testLoginUser testUser1
+                      testSubmitOrganiser organiser1Form_
+                      testAddSchedule addScheduleForm_ location
+                    asNewUser testUser2 $ do
+                      testSubmitOrganiser organiser2Form_
+                      testEditSchedule scheduleUuid_ editScheduleForm_ location
+                      statusIs 403
