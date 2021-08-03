48c0533499bc46ccfe310366fef34dcb70aadf36
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-08-03 18:19:34 +0200

    actually schedule the parties

--- salsa-party-web-server/src/Salsa/Party/Looper/PartyScheduler.hs
+++ salsa-party-web-server/src/Salsa/Party/Looper/PartyScheduler.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
@@ -12,16 +13,17 @@ where
 
 import Conduit
 import Control.Monad
+import Control.Monad.Logger
 import Control.Monad.Reader
 import qualified Data.Conduit.Combinators as C
+import qualified Data.Text as T
 import Data.Time
 import Database.Persist
 import Database.Persist.Sql
 import Salsa.Party.DB
 import Salsa.Party.Web.Server.Foundation
-import Text.Show.Pretty (pPrint)
 
-runPartyScheduler :: (MonadUnliftIO m, MonadReader App m) => m ()
+runPartyScheduler :: (MonadUnliftIO m, MonadLogger m, MonadReader App m) => m ()
 runPartyScheduler = do
   pool <- asks appConnectionPool
   let runDBHere func = runSqlPool func pool
@@ -30,20 +32,20 @@ runPartyScheduler = do
     runConduit $
       scheduleSource
         .| C.mapM (runDBHere . makeScheduleDecision)
-        .| C.mapM_ (liftIO . pPrint)
+        .| scheduleDecisionSink
 
 data ScheduleDecision
   = NextDayTooFarAhead Day
-  | ScheduleAParty Party
+  | ScheduleAParty ScheduleId Party (Maybe ImageId)
   deriving (Show, Eq)
 
 makeScheduleDecision :: MonadUnliftIO m => Entity Schedule -> SqlPersistT m ScheduleDecision
-makeScheduleDecision (Entity scheduleId schedule@Schedule {..}) = do
+makeScheduleDecision (Entity scheduleId_ schedule@Schedule {..}) = do
   -- The last-scheduled party of the same scheduler, or nothing if none has been scheduled yet.
   -- We assume that parties are scheduled in chronological order.
   -- TODO we could get rid of this assumption with a nice join.
   mLastParty <- do
-    mLastScheduleParty <- selectFirst [SchedulePartySchedule ==. scheduleId] [Desc SchedulePartyId]
+    mLastScheduleParty <- selectFirst [SchedulePartySchedule ==. scheduleId_] [Desc SchedulePartyId]
     fmap join $ forM mLastScheduleParty $ \(Entity _ ScheduleParty {..}) -> get schedulePartyParty
   let mLastPartyDay = partyDay <$> mLastParty
   now <- liftIO getCurrentTime
@@ -55,7 +57,8 @@ makeScheduleDecision (Entity scheduleId schedule@Schedule {..}) = do
     then pure $ NextDayTooFarAhead nextDay
     else do
       uuid <- nextRandomUUID
-      pure $ ScheduleAParty (scheduleToPartyOn uuid now nextDay schedule)
+      mImageId <- fmap (schedulePosterImage . entityVal) <$> getBy (UniqueSchedulePoster scheduleId_)
+      pure $ ScheduleAParty scheduleId_ (scheduleToPartyOn uuid now nextDay schedule) mImageId
 
 scheduleToPartyOn :: EventUUID -> UTCTime -> Day -> Schedule -> Party
 scheduleToPartyOn uuid now day Schedule {..} =
@@ -76,3 +79,22 @@ scheduleToPartyOn uuid now day Schedule {..} =
 
 daysToScheduleAhead :: Integer
 daysToScheduleAhead = 45
+
+scheduleDecisionSink :: (MonadUnliftIO m, MonadLogger m, MonadReader App m) => ConduitT ScheduleDecision Void m ()
+scheduleDecisionSink = awaitForever $ \case
+  NextDayTooFarAhead day -> logDebugN $ T.pack $ "Not scheduling a party because the next day would be too far ahead:" <> show day
+  ScheduleAParty scheduleId_ party mImageId -> lift $ do
+    pool <- asks appConnectionPool
+    let runDBHere func = runSqlPool func pool
+    now <- liftIO getCurrentTime
+    runDBHere $ do
+      partyId_ <- insert party
+      insert_ ScheduleParty {schedulePartySchedule = scheduleId_, schedulePartyParty = partyId_, schedulePartyScheduled = now}
+      forM_ mImageId $ \imageId_ ->
+        insert_
+          PartyPoster
+            { partyPosterParty = partyId_,
+              partyPosterImage = imageId_,
+              partyPosterCreated = now,
+              partyPosterModified = Nothing
+            }
--- salsa-party-web-server/test/Salsa/Party/Looper/PartySchedulerSpec.hs
+++ salsa-party-web-server/test/Salsa/Party/Looper/PartySchedulerSpec.hs
@@ -14,54 +14,64 @@ import Test.Syd.Validity
 
 spec :: Spec
 spec = do
-  setupAround salsaConnectionPoolSetupFunc $ do
-    describe "makeScheduleDecision" $ do
-      it "decides to schedule a party if no parties have been organised yet" $ \pool ->
-        forAllValid $ \schedulePrototype ->
-          runPersistentTest pool $ do
-            let schedule = schedulePrototype
-            decision <- makeScheduleDecision schedule
-            case decision of
-              ScheduleAParty _ -> pure ()
-              _ -> liftIO $ expectationFailure $ "Should have decided to schedule a party, but decided this instead: " <> ppShow decision
-      it "decides to schedule a party for the right day in this example case" $ \pool ->
-        forAllValid $ \schedulePrototype ->
-          forAllValid $ \schedulePartyPrototype ->
-            forAllValid $ \partyPrototype ->
-              runPersistentTest pool $ do
-                let recurrence = WeeklyRecurrence Friday
-                    schedule = schedulePrototype {scheduleRecurrence = recurrence}
-                scheduleId <- insert schedule
-                let day = fromGregorian 2021 08 02
-                let party = partyPrototype {partyDay = day}
-                partyId <- insert party
-                let scheduleParty = schedulePartyPrototype {schedulePartySchedule = scheduleId, schedulePartyParty = partyId}
-                insert_ scheduleParty
-                decision <- makeScheduleDecision (Entity scheduleId schedule)
-                liftIO $ case decision of
-                  ScheduleAParty Party {..} -> do
-                    partyDay `shouldBe` fromGregorian 2021 08 06
-                    partyTitle `shouldBe` scheduleTitle schedule
-                    partyDescription `shouldBe` scheduleDescription schedule
-                    partyOrganiser `shouldBe` scheduleOrganiser schedule
-                    partyStart `shouldBe` scheduleStart schedule
-                    partyHomepage `shouldBe` scheduleHomepage schedule
-                    partyPrice `shouldBe` schedulePrice schedule
-                    partyCancelled `shouldBe` False
-                  _ -> liftIO $ expectationFailure $ "Should have decided to schedule a party, but decided this instead: " <> ppShow decision
-      it "decides not to send schedule a party when that party would be too far ahead" $ \pool ->
-        forAllValid $ \schedule ->
-          forAllValid $ \schedulePartyPrototype ->
-            forAllValid $ \partyPrototype ->
-              runPersistentTest pool $ do
-                scheduleId <- insert schedule
-                today <- liftIO $ utctDay <$> getCurrentTime
-                let day = addDays (daysToScheduleAhead + 1) today
-                let party = partyPrototype {partyDay = day}
-                partyId <- insert party
-                let scheduleParty = schedulePartyPrototype {schedulePartySchedule = scheduleId, schedulePartyParty = partyId}
-                insert_ scheduleParty
-                decision <- makeScheduleDecision (Entity scheduleId schedule)
-                liftIO $ case decision of
-                  NextDayTooFarAhead d -> d `shouldSatisfy` (> day)
-                  _ -> expectationFailure $ "Should have decided not to schedule a party, but decided this instead: " <> ppShow decision
+  modifyMaxSuccess (`div` 10) $
+    setupAround salsaConnectionPoolSetupFunc $ do
+      describe "makeScheduleDecision" $ do
+        it "decides to schedule a party if no parties have been organised yet" $ \pool ->
+          forAllValid $ \scheduleEntity ->
+            runPersistentTest pool $ do
+              decision <- makeScheduleDecision scheduleEntity
+              case decision of
+                ScheduleAParty scheduleId_ _ mImageId -> liftIO $ do
+                  scheduleId_ `shouldBe` entityKey scheduleEntity
+                  mImageId `shouldBe` Nothing
+                _ -> liftIO $ expectationFailure $ "Should have decided to schedule a party, but decided this instead: " <> ppShow decision
+
+        it "decides to schedule a party for the right day in this example case" $ \pool ->
+          forAllValid $ \schedulePrototype ->
+            forAllValid $ \schedulePartyPrototype ->
+              forAllValid $ \partyPrototype ->
+                runPersistentTest pool $ do
+                  let recurrence = WeeklyRecurrence Friday
+                      schedule = schedulePrototype {scheduleRecurrence = recurrence}
+                  scheduleId <- insert schedule
+                  let day = fromGregorian 2021 08 02
+                  let party = partyPrototype {partyDay = day}
+                  partyId <- insert party
+                  let scheduleParty =
+                        schedulePartyPrototype
+                          { schedulePartySchedule = scheduleId,
+                            schedulePartyParty = partyId
+                          }
+                  insert_ scheduleParty
+                  decision <- makeScheduleDecision (Entity scheduleId schedule)
+                  liftIO $ case decision of
+                    ScheduleAParty scheduleId' Party {..} mImageId -> do
+                      scheduleId' `shouldBe` scheduleId
+                      partyDay `shouldBe` fromGregorian 2021 08 06
+                      partyTitle `shouldBe` scheduleTitle schedule
+                      partyDescription `shouldBe` scheduleDescription schedule
+                      partyOrganiser `shouldBe` scheduleOrganiser schedule
+                      partyStart `shouldBe` scheduleStart schedule
+                      partyHomepage `shouldBe` scheduleHomepage schedule
+                      partyPrice `shouldBe` schedulePrice schedule
+                      partyCancelled `shouldBe` False
+                      mImageId `shouldBe` Nothing
+                    _ -> liftIO $ expectationFailure $ "Should have decided to schedule a party, but decided this instead: " <> ppShow decision
+
+        it "decides not to send schedule a party when that party would be too far ahead" $ \pool ->
+          forAllValid $ \schedule ->
+            forAllValid $ \schedulePartyPrototype ->
+              forAllValid $ \partyPrototype ->
+                runPersistentTest pool $ do
+                  scheduleId <- insert schedule
+                  today <- liftIO $ utctDay <$> getCurrentTime
+                  let day = addDays (daysToScheduleAhead + 1) today
+                  let party = partyPrototype {partyDay = day}
+                  partyId <- insert party
+                  let scheduleParty = schedulePartyPrototype {schedulePartySchedule = scheduleId, schedulePartyParty = partyId}
+                  insert_ scheduleParty
+                  decision <- makeScheduleDecision (Entity scheduleId schedule)
+                  liftIO $ case decision of
+                    NextDayTooFarAhead d -> d `shouldSatisfy` (> day)
+                    _ -> expectationFailure $ "Should have decided not to schedule a party, but decided this instead: " <> ppShow decision
