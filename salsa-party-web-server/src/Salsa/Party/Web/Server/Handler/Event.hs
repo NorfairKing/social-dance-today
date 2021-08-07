d3330c760ba0ca11338134f91ac65e6abd3fc1c4
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-08-07 02:02:29 +0200

    split up the event route handlers

--- salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Event.hs
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Event.hs
@@ -1,17 +1,13 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module Salsa.Party.Web.Server.Handler.Event
   ( getEventR,
     getEventIcsR,
   )
 where
 
+import Salsa.Party.Web.Server.Handler.Event.ICal
 import Salsa.Party.Web.Server.Handler.ExternalEvent
-import Salsa.Party.Web.Server.Handler.ExternalEvent.ICal
 import Salsa.Party.Web.Server.Handler.Import
 import Salsa.Party.Web.Server.Handler.Party
-import Salsa.Party.Web.Server.Handler.Party.ICal
-import qualified Text.ICalendar as ICal
 
 getEventR :: EventUUID -> Handler Html
 getEventR eventUuid = do
@@ -22,21 +18,4 @@ getEventR eventUuid = do
       mExternalEvent <- runDB $ getBy $ UniqueExternalEventUUID eventUuid
       case mExternalEvent of
         Just externalEventEntity -> externalEventPage externalEventEntity
-        Nothing -> notFound
-
-getEventIcsR :: EventUUID -> Handler ICal.VCalendar
-getEventIcsR eventUuid = do
-  mParty <- runDB $ getBy $ UniquePartyUUID eventUuid
-  case mParty of
-    Just (Entity _ party) -> do
-      place@Place {..} <- runDB $ get404 $ partyPlace party
-      renderUrl <- getUrlRender
-      pure $ partyCalendar renderUrl party place
-    Nothing -> do
-      mExternalEvent <- runDB $ getBy $ UniqueExternalEventUUID eventUuid
-      case mExternalEvent of
-        Just (Entity _ externalEvent) -> do
-          place@Place {..} <- runDB $ get404 $ externalEventPlace externalEvent
-          renderUrl <- getUrlRender
-          pure $ externalEventCalendar renderUrl externalEvent place
         Nothing -> notFound
---
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Event/ICal.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Salsa.Party.Web.Server.Handler.Event.ICal
+  ( getEventIcsR,
+  )
+where
+
+import Salsa.Party.Web.Server.Handler.ExternalEvent.ICal
+import Salsa.Party.Web.Server.Handler.Import
+import Salsa.Party.Web.Server.Handler.Party.ICal
+import qualified Text.ICalendar as ICal
+
+getEventIcsR :: EventUUID -> Handler ICal.VCalendar
+getEventIcsR eventUuid = do
+  mParty <- runDB $ getBy $ UniquePartyUUID eventUuid
+  case mParty of
+    Just (Entity _ party) -> do
+      place@Place {..} <- runDB $ get404 $ partyPlace party
+      renderUrl <- getUrlRender
+      pure $ partyCalendar renderUrl party place
+    Nothing -> do
+      mExternalEvent <- runDB $ getBy $ UniqueExternalEventUUID eventUuid
+      case mExternalEvent of
+        Just (Entity _ externalEvent) -> do
+          place@Place {..} <- runDB $ get404 $ externalEventPlace externalEvent
+          renderUrl <- getUrlRender
+          pure $ externalEventCalendar renderUrl externalEvent place
+        Nothing -> notFound
