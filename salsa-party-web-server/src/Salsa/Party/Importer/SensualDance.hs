fdb00ef56fccc3cef9fee132fc7e599083c47290
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-08-13 10:53:21 +0200

    this is going to be extremely complex to pull off

--- salsa-party-web-server/src/Salsa/Party/Importer/SensualDance.hs
+++ salsa-party-web-server/src/Salsa/Party/Importer/SensualDance.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 -- | https://sensual.dance
 --
@@ -10,12 +8,16 @@ module Salsa.Party.Importer.SensualDance (sensualDanceImporter) where
 
 import Conduit
 import Data.Aeson
+import qualified Data.ByteString.Lazy as LB
 import qualified Data.Conduit.Combinators as C
 import Data.Maybe
 import qualified Data.Text as T
 import Network.HTTP.Client as HTTP
+import Network.URI
 import Salsa.Party.Importer.Import
 import Salsa.Party.Web.Server.Geocoding
+import Text.HTML.Scalpel
+import Text.HTML.Scalpel.Extended
 
 sensualDanceImporter :: Importer
 sensualDanceImporter =
@@ -28,4 +30,28 @@ func :: Import ()
 func = do
   runConduit $
     yield "http://sensual.dance/"
+      .| C.concatMap (parseRequest :: String -> Maybe HTTP.Request)
+      .| doHttpRequestWith
+      .| logRequestErrors
+      .| findScripts
+      .| C.concatMap (\u -> requestFromURI u :: Maybe HTTP.Request)
+      .| doHttpRequestWith
+      .| logRequestErrors
       .| C.mapM_ (liftIO . pPrint)
+
+findScripts :: ConduitT (HTTP.Request, HTTP.Response LB.ByteString) URI Import ()
+findScripts = awaitForever $ \(request, response) -> do
+  let linkScraper = do
+        refs1 <- attrs "href" "a"
+        refs2 <- attrs "src" "script"
+        pure $ mapMaybe maybeUtf8 $ refs1 ++ refs2
+  case scrapeStringLike (responseBody response) linkScraper of
+    Nothing -> pure ()
+    Just links ->
+      yieldMany $
+        mapMaybe
+          ( \t -> do
+              refURI <- parseURIReference (T.unpack t)
+              pure $ refURI `relativeTo` getUri request
+          )
+          links
