e7d12c17d8cff49cd218b52de23cf00cb6b2abd5
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-08-18 17:08:45 +0200

    test to make sure that no messages are unused

--- salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/I18NSpec.hs
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/I18NSpec.hs
@@ -3,10 +3,14 @@
 
 module Salsa.Party.Web.Server.Handler.I18NSpec (spec) where
 
+import Data.ByteString (ByteString)
 import qualified Data.ByteString as SB
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
 import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as S
+import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import Path
@@ -24,6 +28,25 @@ spec =
       Left _ -> liftIO $ die "main file did not contain valid utf8"
       Right mainContents -> do
         let mainMessages = messagesIn (T.unpack mainContents)
+
+        let dirs :: [FilePath]
+            dirs = ["src", "templates"]
+        usageMap <- liftIO $ do
+          files <- fmap concat $
+            forM dirs $ \dir -> do
+              adir <- resolveDir' dir
+              snd <$> listDirRecur adir
+          fmap (M.unionsWith (+)) $
+            forM files $ \file -> do
+              contents <- SB.readFile $ fromAbsFile file
+              pure $ usageMapFor mainMessages contents
+
+        forM_ mainMessages $ \mainMessage ->
+          it (show mainMessage <> " is used") $ case M.lookup mainMessage usageMap of
+            Just n | n > 0 -> pure ()
+            _ -> expectationFailure $ unwords ["Message is unused:", show mainMessage]
+
+        -- For each translation
         scenarioDir "messages" $ \fp -> do
           otherFileContents <- liftIO $ SB.readFile fp
           case TE.decodeUtf8' otherFileContents of
@@ -34,19 +57,27 @@ spec =
               describe (fp <> " has translations for every string") $ do
                 let otherMessages = messagesIn (T.unpack otherContents)
                 forM_ mainMessages $ \mainMessage ->
-                  it ("Has a translation for " <> mainMessage) $
+                  it ("Has a translation for " <> show mainMessage) $
                     if S.member mainMessage otherMessages
                       then pure ()
                       else
                         expectationFailure $
                           unwords
                             [ "Translation for",
-                              mainMessage,
+                              show mainMessage,
                               "not found in",
                               fp
                             ]
 
-messagesIn :: String -> Set String
+usageMapFor :: Set Text -> ByteString -> Map Text Word
+usageMapFor messages contents = case TE.decodeUtf8' contents of
+  Left _ -> M.empty
+  Right textContents -> flip M.fromSet messages $ \message ->
+    if ("Msg" <> message) `T.isInfixOf` textContents
+      then 1
+      else 0
+
+messagesIn :: String -> Set Text
 messagesIn contents =
   let firstWords :: [String]
       firstWords = mapMaybe (listToMaybe . words) $ lines contents
@@ -54,4 +85,4 @@ messagesIn contents =
         ':' : s -> s
         s -> s
       dropColumn = reverse . stripColumn . reverse
-   in S.fromList $ map dropColumn $ filter (not . all (== '#')) firstWords
+   in S.fromList $ map (T.pack . dropColumn) $ filter (not . all (== '#')) firstWords
