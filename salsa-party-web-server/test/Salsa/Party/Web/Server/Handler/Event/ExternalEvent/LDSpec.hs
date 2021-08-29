3bda153bba1e6a738b4aa5d9b2ce49ff61e2c137
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-08-30 00:11:00 +0200

    validity tests for the linked data output

--- salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/Event/ExternalEvent/LDSpec.hs
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/Event/ExternalEvent/LDSpec.hs
@@ -14,42 +14,56 @@ import Test.Syd.Aeson
 import Yesod.Core
 
 spec :: Spec
-spec = do
-  let exampleExternalEvent =
-        ExternalEvent
-          { externalEventUuid = Typed.UUID $ UUID.fromWords 123 456 789 101112,
-            externalEventKey = "suavemente-cuban-party-2021-07-16-kultur-bistro-bern",
-            externalEventTitle = "Suavemente Cuban Party",
-            externalEventDescription = Just "Cuban Salsa Party\r\n\r\n20:00 Door open\r\n20:30 Cuban Salsa Workshop\r\n21:30 Cuban Party\r\n23:30 Animation\r\n\n\nhttps://salsaluca.ch/index.php/events",
-            externalEventOrganiser = Just "Kultur Bistro",
-            externalEventDay = fromGregorian 2021 07 16,
-            externalEventStart = Just (TimeOfDay 20 15 00),
-            externalEventHomepage = Nothing,
-            externalEventPrice = Just "15.0 CHF",
-            externalEventCancelled = False,
-            externalEventCreated = UTCTime (fromGregorian 2021 07 05) 185621,
-            externalEventModified = Nothing,
-            externalEventPlace = toSqlKey 0,
-            externalEventImporter = toSqlKey 0,
-            externalEventOrigin = "https://events.info/events/suavemente-cuban-party-2021-07-16-kultur-bistro-bern"
-          }
+spec =
+  modifyMaxSuccess (`div` 20) $
+    modifyMaxSize (* 10) $
+      appSpec $ do
+        it "always outputs valid json (without crashing)" $ \app ->
+          forAllValid $ \externalEvent ->
+            forAllValid $ \place ->
+              forAllValid $ \mCasKey ->
+                let urlRender :: Route App -> Text
+                    urlRender route = yesodRender app "https://social-dance.today" route []
+                 in shouldBeValid $
+                      externalEventToLDEvent
+                        urlRender
+                        externalEvent
+                        place
+                        mCasKey
 
-  let examplePlace =
-        Place
-          { placeQuery = "Bahnhofplatz 6207 Nottwil LU",
-            placeLat = Latitude 47.138657700,
-            placeLon = Longitude 8.138471299
-          }
+        it "outputs the same JSON LD as before for this external event" $ \app ->
+          let exampleExternalEvent =
+                ExternalEvent
+                  { externalEventUuid = Typed.UUID $ UUID.fromWords 123 456 789 101112,
+                    externalEventKey = "suavemente-cuban-party-2021-07-16-kultur-bistro-bern",
+                    externalEventTitle = "Suavemente Cuban Party",
+                    externalEventDescription = Just "Cuban Salsa Party\r\n\r\n20:00 Door open\r\n20:30 Cuban Salsa Workshop\r\n21:30 Cuban Party\r\n23:30 Animation\r\n\n\nhttps://salsaluca.ch/index.php/events",
+                    externalEventOrganiser = Just "Kultur Bistro",
+                    externalEventDay = fromGregorian 2021 07 16,
+                    externalEventStart = Just (TimeOfDay 20 15 00),
+                    externalEventHomepage = Nothing,
+                    externalEventPrice = Just "15.0 CHF",
+                    externalEventCancelled = False,
+                    externalEventCreated = UTCTime (fromGregorian 2021 07 05) 185621,
+                    externalEventModified = Nothing,
+                    externalEventPlace = toSqlKey 0,
+                    externalEventImporter = toSqlKey 0,
+                    externalEventOrigin = "https://events.info/events/suavemente-cuban-party-2021-07-16-kultur-bistro-bern"
+                  }
 
-  appSpec $
-    it "outputs the same JSON LD as before for this external event" $ \app ->
-      let urlRender :: Route App -> Text
-          urlRender route = yesodRender app "https://social-dance.today" route []
-       in pureGoldenJSONValueFile
-            "test_resources/ld/external-event.json"
-            ( externalEventToLDEvent
-                urlRender
-                exampleExternalEvent
-                examplePlace
-                Nothing
-            )
+              examplePlace =
+                Place
+                  { placeQuery = "Bahnhofplatz 6207 Nottwil LU",
+                    placeLat = Latitude 47.138657700,
+                    placeLon = Longitude 8.138471299
+                  }
+              urlRender :: Route App -> Text
+              urlRender route = yesodRender app "https://social-dance.today" route []
+           in pureGoldenJSONValueFile
+                "test_resources/ld/external-event.json"
+                ( externalEventToLDEvent
+                    urlRender
+                    exampleExternalEvent
+                    examplePlace
+                    (either (const Nothing) Just $ parseCASKey "UTpq9WwrRgBrNo9GusMO2QYGN+IZCK4E+IsnbgCVmvY=")
+                )
--- salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/Event/Party/LDSpec.hs
+++ salsa-party-web-server/test/Salsa/Party/Web/Server/Handler/Event/Party/LDSpec.hs
@@ -14,50 +14,66 @@ import Test.Syd.Aeson
 import Yesod.Core
 
 spec :: Spec
-spec = do
-  let exampleOrganiser =
-        Organiser
-          { organiserUuid = Typed.UUID $ UUID.fromWords 123 456 789 101112,
-            organiserUser = toSqlKey 0,
-            organiserName = "CS SYD",
-            organiserHomepage = Just "https://cs-syd.eu",
-            organiserCreated = UTCTime (fromGregorian 2021 06 19) 164155,
-            organiserModified = Nothing
-          }
+spec =
+  modifyMaxSuccess (`div` 20) $
+    modifyMaxSize (* 10) $
+      appSpec $ do
+        it "always outputs valid json (without crashing)" $ \app ->
+          forAllValid $ \organiser ->
+            forAllValid $ \party ->
+              forAllValid $ \place ->
+                forAllValid $ \mCasKey ->
+                  let urlRender :: Route App -> Text
+                      urlRender route = yesodRender app "https://social-dance.today" route []
+                   in shouldBeValid $
+                        partyToLDEvent
+                          urlRender
+                          party
+                          organiser
+                          place
+                          mCasKey
 
-  let exampleParty =
-        Party
-          { partyUuid = Typed.UUID $ UUID.fromWords 123 456 789 101112,
-            partyOrganiser = toSqlKey 0,
-            partyTitle = "Example party at Rhythmia",
-            partyDescription = Just "aeou\r\naoseuntha\r\noeu",
-            partyDay = fromGregorian 2021 06 15,
-            partyStart = Nothing,
-            partyHomepage = Just "https://www.rhythmia.ch/",
-            partyPrice = Just "5 CHF",
-            partyCancelled = True,
-            partyCreated = UTCTime (fromGregorian 2021 06 19) 164155,
-            partyModified = Nothing,
-            partyPlace = toSqlKey 0
-          }
+        it "outputs the same JSON LD as before for this party" $ \app ->
+          let exampleOrganiser =
+                Organiser
+                  { organiserUuid = Typed.UUID $ UUID.fromWords 123 456 789 101112,
+                    organiserUser = toSqlKey 0,
+                    organiserName = "CS SYD",
+                    organiserHomepage = Just "https://cs-syd.eu",
+                    organiserCreated = UTCTime (fromGregorian 2021 06 19) 164155,
+                    organiserModified = Nothing
+                  }
 
-  let examplePlace =
-        Place
-          { placeQuery = "Spitalgasse 4, 3011 Bern Bern",
-            placeLat = Latitude 46.948335899,
-            placeLon = Longitude 7.443078400
-          }
+              exampleParty =
+                Party
+                  { partyUuid = Typed.UUID $ UUID.fromWords 123 456 789 101112,
+                    partyOrganiser = toSqlKey 0,
+                    partyTitle = "Example party at Rhythmia",
+                    partyDescription = Just "aeou\r\naoseuntha\r\noeu",
+                    partyDay = fromGregorian 2021 06 15,
+                    partyStart = Nothing,
+                    partyHomepage = Just "https://www.rhythmia.ch/",
+                    partyPrice = Just "5 CHF",
+                    partyCancelled = True,
+                    partyCreated = UTCTime (fromGregorian 2021 06 19) 164155,
+                    partyModified = Nothing,
+                    partyPlace = toSqlKey 0
+                  }
 
-  appSpec $
-    it "outputs the same JSON LD as before for this party" $ \app ->
-      let urlRender :: Route App -> Text
-          urlRender route = yesodRender app "https://social-dance.today" route []
-       in pureGoldenJSONValueFile
-            "test_resources/ld/party.json"
-            ( partyToLDEvent
-                urlRender
-                exampleParty
-                exampleOrganiser
-                examplePlace
-                (either (const Nothing) Just $ parseCASKey "UTpq9WwrRgBrNo9GusMO2QYGN+IZCK4E+IsnbgCVmvY=")
-            )
+              examplePlace =
+                Place
+                  { placeQuery = "Spitalgasse 4, 3011 Bern Bern",
+                    placeLat = Latitude 46.948335899,
+                    placeLon = Longitude 7.443078400
+                  }
+              urlRender :: Route App -> Text
+              urlRender route = yesodRender app "https://social-dance.today" route []
+           in pureGoldenJSONValueFile
+                "test_resources/ld/party.json"
+                ( partyToLDEvent
+                    urlRender
+                    exampleParty
+                    exampleOrganiser
+                    examplePlace
+                    (either (const Nothing) Just $ parseCASKey "UTpq9WwrRgBrNo9GusMO2QYGN+IZCK4E+IsnbgCVmvY=")
+                )
