78983d4fdfd2ee75e1230deca1480de4beb46047
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-08-30 17:36:00 +0200

    more messing about with niklas

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -37,7 +37,8 @@ in
             enableExecutableProfiling = false;
             preConfigure = (old.preConfigure or "") + ''
               echo DISPLAY = $DISPLAY
-              ${final.strace}/bin/strace -s 3000 -o strace.log -fy ${/tmp/nix-build-salsa-party-web-server-0.0.0.0.drv-7/source/dist/build/salsa-party-web-server-test/salsa-party-web-server-test} --match=HomeSpec
+              # export EXTRA_CHROMIUM_ARGS=""
+              ${final.strace}/bin/strace -s 3000 -o strace.log -fy ${/tmp/nix-build-salsa-party-web-server-0.0.0.0.drv-14/source/dist/build/salsa-party-web-server-test/salsa-party-web-server-test} --match=HomeSpec
               exit 1
             '';
             buildDepends = (old.buildInputs or [ ]) ++ (with final; [
