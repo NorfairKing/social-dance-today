92dd0890ad5df84f3ebe9b273a78c2f793752ec6
Tom Sydney Kerckhove <syd@cs-syd.eu>
2021-09-13 11:11:55 +0200

    refactor party query out into module

--- salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Event/Party.hs
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Event/Party.hs
@@ -1,25 +1,18 @@
 module Salsa.Party.Web.Server.Handler.Event.Party (getPartySlugR, partyPage) where
 
-import qualified Database.Esqueleto as E
 import Salsa.Party.Web.Server.Handler.Event.Party.HTML
 import Salsa.Party.Web.Server.Handler.Event.Party.ICal
 import Salsa.Party.Web.Server.Handler.Event.Party.JSON
 import Salsa.Party.Web.Server.Handler.Event.Party.LD
+import Salsa.Party.Web.Server.Handler.Event.Party.Query
 import Salsa.Party.Web.Server.Handler.Import
 
 getPartySlugR :: OrganiserSlug -> EventSlug -> Day -> Handler TypedContent
 getPartySlugR organiserSlug_ partySlug_ day = do
-  mPartyEntity <- runDB $
-    selectOne $
-      E.from $ \(organiser `E.InnerJoin` party) -> do
-        E.on $ party E.^. PartyOrganiser E.==. organiser E.^. OrganiserId
-        E.where_ $ party E.^. PartyDay E.==. E.val day
-        E.where_ $ organiser E.^. OrganiserSlug E.==. E.just (E.val organiserSlug_)
-        E.where_ $ party E.^. PartySlug E.==. E.just (E.val partySlug_)
-        pure party
-  case mPartyEntity of
+  mPartyTup <- runDB $ getPartyTupBySlug organiserSlug_ partySlug_ day
+  case mPartyTup of
     Nothing -> notFound
-    Just partyEntity -> do
+    Just (_, partyEntity) -> do
       -- TODO we can make this much more efficient if we fetch the organiser only once.
       partyPage partyEntity
 
---
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Event/Party/Query.hs
@@ -0,0 +1,19 @@
+module Salsa.Party.Web.Server.Handler.Event.Party.Query (getPartyTupBySlug) where
+
+import Control.Monad.IO.Class
+import Data.Time
+import qualified Database.Esqueleto as E
+import Database.Persist
+import Database.Persist.Sql
+import Salsa.Party.DB
+import Salsa.Party.Web.Server.Foundation
+
+getPartyTupBySlug :: MonadIO m => OrganiserSlug -> EventSlug -> Day -> SqlPersistT m (Maybe (Entity Organiser, Entity Party))
+getPartyTupBySlug organiserSlug_ partySlug_ day = do
+  selectOne $
+    E.from $ \(organiser `E.InnerJoin` party) -> do
+      E.on $ party E.^. PartyOrganiser E.==. organiser E.^. OrganiserId
+      E.where_ $ party E.^. PartyDay E.==. E.val day
+      E.where_ $ organiser E.^. OrganiserSlug E.==. E.just (E.val organiserSlug_)
+      E.where_ $ party E.^. PartySlug E.==. E.just (E.val partySlug_)
+      pure (organiser, party)
