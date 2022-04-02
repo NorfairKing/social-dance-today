2620177db33c298fc287ca67d456a8ad85cf7197
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-04-02 12:31:20 +0200

    no need for setting up slugs anymore

--- salsa-party-web-server/src/Salsa/Party/DB/Migration.hs
+++ salsa-party-web-server/src/Salsa/Party/DB/Migration.hs
@@ -13,7 +13,6 @@ import Conduit
 import Control.Monad
 import Control.Monad.Logger
 import Data.ByteString (ByteString)
-import qualified Data.Conduit.Combinators as C
 import Data.FileEmbed
 import qualified Data.Text as T
 import Data.Validity (Validity)
@@ -21,7 +20,6 @@ import Data.Yaml as Yaml
 import Database.Persist.Sql
 import GHC.Generics (Generic)
 import Salsa.Party.DB
-import Salsa.Party.Web.Server.Foundation
 import System.Exit
 import UnliftIO
 
@@ -35,7 +33,6 @@ completeServerMigration quiet = do
             )
   logInfoN "Autmatic migrations done, starting application-specific migrations."
   setUpPlaces
-  setUpSlugs
   logInfoN "Migrations done."
 
 data Location = Location
@@ -64,44 +61,6 @@ setUpPlaces = do
       [ PlaceLat =. placeLat locationPlace,
         PlaceLon =. placeLon locationPlace
       ]
-
-setUpSlugs :: (MonadUnliftIO m, MonadLogger m) => SqlPersistT m ()
-setUpSlugs = do
-  logInfoN "Setting up slugs for slugless events"
-  setUpOrganiserSlugs
-  setUpPartySlugs
-  setUpExternalEventSlugs
-
-setUpOrganiserSlugs :: MonadUnliftIO m => SqlPersistT m ()
-setUpOrganiserSlugs = do
-  -- Don't update slugs, otherwise urls might stop working behind organisers' backs.
-  ackOrganiserSource <- selectSourceRes [OrganiserSlug ==. Nothing] []
-  withAcquire ackOrganiserSource $ \organiserSource ->
-    runConduit $ organiserSource .| C.mapM_ setupOrganiserSlug
-
-setupOrganiserSlug :: MonadIO m => Entity Organiser -> SqlPersistT m ()
-setupOrganiserSlug (Entity organiserId Organiser {..}) = update organiserId [OrganiserSlug =. makeOrganiserSlug organiserName]
-
-setUpPartySlugs :: MonadUnliftIO m => SqlPersistT m ()
-setUpPartySlugs = do
-  -- Don't update slugs, otherwise urls might stop working behind organisers' backs.
-  ackPartySource <- selectSourceRes [PartySlug ==. Nothing] []
-  withAcquire ackPartySource $ \partySource ->
-    runConduit $ partySource .| C.mapM_ setupPartySlug
-
-setupPartySlug :: MonadIO m => Entity Party -> SqlPersistT m ()
-setupPartySlug (Entity partyId Party {..}) = update partyId [PartySlug =. makePartySlug partyTitle]
-
-setUpExternalEventSlugs :: MonadUnliftIO m => SqlPersistT m ()
-setUpExternalEventSlugs = do
-  -- Don't update slugs, otherwise urls might stop working behind organisers' backs.
-  ackExternalEventSource <- selectSourceRes [ExternalEventSlug ==. Nothing] []
-  withAcquire ackExternalEventSource $ \externalEventSource ->
-    runConduit $ externalEventSource .| C.mapM_ setupExternalEventSlug
-
-setupExternalEventSlug :: MonadIO m => Entity ExternalEvent -> SqlPersistT m ()
-setupExternalEventSlug (Entity externalEventId ExternalEvent {..}) =
-  update externalEventId [ExternalEventSlug =. makeExternalEventSlug externalEventUuid externalEventTitle]
 
 {-# NOINLINE locations #-}
 locations :: [Location]
