6d25eb13c7487b97f980743d273bcb1985b76cf4
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-06-21 17:46:29 +0200

    Don't geocode invalid places

--- salsa-party-web-server/src/Salsa/Party/Web/Server/Geocoding.hs
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Geocoding.hs
@@ -12,6 +12,7 @@ import Control.Monad.Reader
 import Data.Maybe
 import Data.Text (Text)
 import qualified Data.Text as T
+import Data.Validity
 import Database.Persist
 import qualified Google.Geocoding as Google
 import qualified OpenStreetMaps.Geocoding as OSM
@@ -30,65 +31,96 @@ lookupPlace query = do
 
 lookupPlaceRaw :: (MonadUnliftIO m, MonadReader App m, MonadLoggerIO m) => Text -> m (Maybe (Entity Place))
 lookupPlaceRaw query = do
-  mPlace <- appDB $ getBy $ UniquePlaceQuery query
-  case mPlace of
-    Just pe -> do
-      logDebugNS "geocoding" $ "Found place in cache, not gecoding query: " <> query
-      pure $ Just pe
-    Nothing -> do
-      logDebugNS "geocoding" $ "Did not find place in cache, gecoding query: " <> query
-
-      mOSMRateLimiter <- asks appOSMRateLimiter
-      mGoogleAPIKey <- asks appGoogleAPIKey
-
-      mCoordinates <- case (mOSMRateLimiter, mGoogleAPIKey) of
-        (Nothing, Nothing) -> do
-          let msg =
-                T.concat
-                  [ "No geocoding service configured to geocode ",
-                    T.pack (show query),
-                    ", please contact the site administrators."
-                  ]
-          logErrorNS "geocoding" msg
-          sendAdminNotification msg
-          pure Nothing
-        (Just osmRateLimiter, Nothing) -> do
-          liftIO $ waitDebit osmRateLimiter 1
-          geocodeviaOSM query
-        (Nothing, Just googleAPIKey) -> do
-          geocodeViaGoogle googleAPIKey query
-        (Just osmRateLimiter, Just googleAPIKey) -> do
-          debitSucceeded <- liftIO $ tryDebit osmRateLimiter 1
-          mCoords <-
-            if debitSucceeded
-              then do
-                mOSMResult <- geocodeviaOSM query
-                case mOSMResult of
-                  Just _ -> pure mOSMResult
-                  -- Try using google if OSM fails.
-                  Nothing -> geocodeViaGoogle googleAPIKey query
-              else pure Nothing
-          case mCoords of
-            Just coords -> pure $ Just coords
-            Nothing -> geocodeViaGoogle googleAPIKey query
-
-      case mCoordinates of
-        Nothing -> pure Nothing
-        Just coordinates@Coordinates {..} -> do
-          logInfoNS "geocoding" $ "Geocoded " <> query <> " to " <> T.pack (show coordinates)
-          fmap Just $
-            appDB $
-              upsertBy
-                (UniquePlaceQuery query)
-                ( Place
-                    { placeQuery = query,
-                      placeLat = coordinatesLat,
-                      placeLon = coordinatesLon
-                    }
-                )
-                [ PlaceLat =. coordinatesLat,
-                  PlaceLon =. coordinatesLon
+  if T.null query
+    then do
+      logWarnNS "geocoding" $
+        T.pack $
+          unwords
+            [ "Not geocoding because the query is empty:",
+              show query
+            ]
+      pure Nothing
+    else do
+      mPlace <- appDB $ getBy $ UniquePlaceQuery query
+      case mPlace of
+        Just pe -> do
+          logDebugNS "geocoding" $
+            T.pack $
+              unwords
+                [ "Found place in cache, not gecoding query:",
+                  show query
                 ]
+          pure $ Just pe
+        Nothing -> do
+          logDebugNS "geocoding" $
+            T.pack $
+              unwords
+                [ "Did not find place in cache, gecoding query:",
+                  show query
+                ]
+
+          mOSMRateLimiter <- asks appOSMRateLimiter
+          mGoogleAPIKey <- asks appGoogleAPIKey
+
+          mCoordinates <- case (mOSMRateLimiter, mGoogleAPIKey) of
+            (Nothing, Nothing) -> do
+              let msg =
+                    T.concat
+                      [ "No geocoding service configured to geocode ",
+                        T.pack (show query),
+                        ", please contact the site administrators."
+                      ]
+              logErrorNS "geocoding" msg
+              sendAdminNotification msg
+              pure Nothing
+            (Just osmRateLimiter, Nothing) -> do
+              liftIO $ waitDebit osmRateLimiter 1
+              geocodeviaOSM query
+            (Nothing, Just googleAPIKey) -> do
+              geocodeViaGoogle googleAPIKey query
+            (Just osmRateLimiter, Just googleAPIKey) -> do
+              debitSucceeded <- liftIO $ tryDebit osmRateLimiter 1
+              mCoords <-
+                if debitSucceeded
+                  then do
+                    mOSMResult <- geocodeviaOSM query
+                    case mOSMResult of
+                      Just _ -> pure mOSMResult
+                      -- Try using google if OSM fails.
+                      Nothing -> geocodeViaGoogle googleAPIKey query
+                  else pure Nothing
+              case mCoords of
+                Just coords -> pure $ Just coords
+                Nothing -> geocodeViaGoogle googleAPIKey query
+
+          case mCoordinates of
+            Nothing -> pure Nothing
+            Just coordinates@Coordinates {..} -> do
+              logInfoNS "geocoding" $ T.pack $ unwords ["Geocoded", show query, "to", show coordinates]
+              let place =
+                    Place
+                      { placeQuery = query,
+                        placeLat = coordinatesLat,
+                        placeLon = coordinatesLon
+                      }
+              case prettyValidate place of
+                Left err -> do
+                  logErrorNS "geocoding" $
+                    T.pack $
+                      unlines
+                        [ unwords ["Geocoding of", show query, "failed because it produced an invalid place"],
+                          err
+                        ]
+                  pure Nothing
+                Right _ -> do
+                  fmap Just $
+                    appDB $
+                      upsertBy
+                        (UniquePlaceQuery query)
+                        place
+                        [ PlaceLat =. coordinatesLat,
+                          PlaceLon =. coordinatesLon
+                        ]
 
 geocodeviaOSM :: (MonadReader App m, MonadLogger m, MonadIO m) => Text -> m (Maybe Coordinates)
 geocodeviaOSM query = do
