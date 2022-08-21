d950bed2af2f74274fbb8b950a3271b906e6d064
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-08-21 15:36:27 +0200

    cache headers for logo and favicon

--- salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Favicon.hs
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Favicon.hs
@@ -10,6 +10,8 @@ import Data.FileEmbed
 import Salsa.Party.Web.Server.Handler.Import
 
 getFaviconR :: Handler TypedContent
-getFaviconR =
+getFaviconR = do
+  addHeader "Cache-Control" "max-age=31536000, public, immutable"
+  addHeader "Content-Disposition" "inline"
   -- https://stackoverflow.com/questions/13827325/correct-mime-type-for-favicon-ico
   respond "image/x-icon" $(makeRelativeToProject "assets/favicon.ico" >>= embedFile)
--- salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Logo.hs
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Logo.hs
@@ -10,6 +10,8 @@ import Data.FileEmbed
 import Salsa.Party.Web.Server.Handler.Import
 
 getLogoR :: Handler TypedContent
-getLogoR =
+getLogoR = do
+  addHeader "Cache-Control" "max-age=31536000, public, immutable"
+  addHeader "Content-Disposition" "inline"
   -- https://www.w3.org/TR/SVG11/intro.html#MIMEType
   respond "image/svg+xml" $(makeRelativeToProject "assets/logo.svg" >>= embedFile)
