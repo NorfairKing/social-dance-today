8bb774a2b38ac5e14786acf078fc290de586850f
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-09-15 08:28:23 +0200

    Stacked lines

--- salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Admin/Charts.hs
+++ salsa-party-web-server/src/Salsa/Party/Web/Server/Handler/Admin/Charts.hs
@@ -13,9 +13,11 @@ module Salsa.Party.Web.Server.Handler.Admin.Charts
 where
 
 import Conduit
+import Control.Arrow (first)
 import qualified Data.Conduit.Combinators as C
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
+import qualified Data.Text as T
 import Data.Word
 import Safe
 import Salsa.Party.Web.Server.Handler.Import
@@ -31,26 +33,26 @@ getAdminChartsR = do
 
   -- Upcoming events
   dayCountMapOfExternalEvents <- makeUpcomingEventsMap today acqExternalEventsSource $ \(Entity _ ExternalEvent {..}) ->
-    (utctDay externalEventCreated, externalEventDay)
+    (Just externalEventImporter, (utctDay externalEventCreated, externalEventDay))
 
   dayCountMapOfParties <- makeUpcomingEventsMap today acqPartiesSource $ \(Entity _ Party {..}) ->
-    (utctDay partyCreated, partyDay)
+    (Nothing, (utctDay partyCreated, partyDay))
 
   let dayCountMapOfTotal = combineUpcomingEventsMap dayCountMapOfParties dayCountMapOfExternalEvents
 
   dayCountMapOfExternalEvents30Days <- makeUpcomingEventsMapWithLimit today 30 acqExternalEventsSource $ \(Entity _ ExternalEvent {..}) ->
-    (utctDay externalEventCreated, externalEventDay)
+    (Just externalEventImporter, (utctDay externalEventCreated, externalEventDay))
 
   dayCountMapOfParties30Days <- makeUpcomingEventsMapWithLimit today 30 acqPartiesSource $ \(Entity _ Party {..}) ->
-    (utctDay partyCreated, partyDay)
+    (Nothing, (utctDay partyCreated, partyDay))
 
   let dayCountMapOfTotal30Days = combineUpcomingEventsMap dayCountMapOfParties30Days dayCountMapOfExternalEvents30Days
 
   dayCountMapOfExternalEvents7Days <- makeUpcomingEventsMapWithLimit today 7 acqExternalEventsSource $ \(Entity _ ExternalEvent {..}) ->
-    (utctDay externalEventCreated, externalEventDay)
+    (Just externalEventImporter, (utctDay externalEventCreated, externalEventDay))
 
   dayCountMapOfParties7Days <- makeUpcomingEventsMapWithLimit today 7 acqPartiesSource $ \(Entity _ Party {..}) ->
-    (utctDay partyCreated, partyDay)
+    (Nothing, (utctDay partyCreated, partyDay))
 
   let dayCountMapOfTotal7Days = combineUpcomingEventsMap dayCountMapOfParties7Days dayCountMapOfExternalEvents7Days
 
@@ -73,6 +75,15 @@ getAdminChartsR = do
     addScriptRemote "https://cdn.jsdelivr.net/npm/chart.js@3.4.1/dist/chart.min.js"
     $(widgetFile "admin/charts")
 
+currentAndLastMonthStackedCountWidget :: Map Day (Map b Word64) -> Widget
+currentAndLastMonthStackedCountWidget m = do
+  today <- getClientToday
+  let nowCount :: Word64
+      nowCount = maybe 0 sum $ M.lookup today m
+  let lastMonthCount :: Word64
+      lastMonthCount = maybe 0 sum $ M.lookup (addDays (-30) today) m
+  currentAndLastWidget nowCount lastMonthCount
+
 currentAndLastMonthCountWidget :: Map Day Word64 -> Widget
 currentAndLastMonthCountWidget m = do
   today <- getClientToday
@@ -80,6 +91,10 @@ currentAndLastMonthCountWidget m = do
       nowCount = fromMaybe 0 $ M.lookup today m
   let lastMonthCount :: Word64
       lastMonthCount = fromMaybe 0 $ M.lookup (addDays (-30) today) m
+  currentAndLastWidget nowCount lastMonthCount
+
+currentAndLastWidget :: Word64 -> Word64 -> Widget
+currentAndLastWidget nowCount lastMonthCount = do
   let increaseRatio :: Double
       increaseRatio = (fromIntegral nowCount - fromIntegral lastMonthCount) / fromIntegral lastMonthCount
   let increasePercentageString :: String
@@ -110,42 +125,90 @@ addToDayMap today m created =
       mapOfOnes = M.fromAscList $ map (\d -> (d, 1)) days
    in M.unionWith (+) mapOfOnes m
 
-combineUpcomingEventsMap :: Map Day Word64 -> Map Day Word64 -> Map Day Word64
-combineUpcomingEventsMap = M.unionWith (+)
+combineUpcomingEventsMap ::
+  Map Day (Map (Maybe ImporterMetadataId) Word64) ->
+  Map Day (Map (Maybe ImporterMetadataId) Word64) ->
+  Map Day (Map (Maybe ImporterMetadataId) Word64)
+combineUpcomingEventsMap = M.unionWith (M.unionWith (+))
 
-makeUpcomingEventsMap :: MonadUnliftIO m => Day -> Acquire (ConduitT () a m ()) -> (a -> (Day, Day)) -> m (Map Day Word64)
+makeUpcomingEventsMap ::
+  MonadUnliftIO m =>
+  Day ->
+  Acquire (ConduitT () a m ()) ->
+  (a -> (Maybe ImporterMetadataId, (Day, Day))) ->
+  m (Map Day (Map (Maybe ImporterMetadataId) Word64))
 makeUpcomingEventsMap today source func =
   makeDayMap
     source
     ( \m a ->
-        let (created, scheduled) = func a
-         in addToUpcomingEventsMap today m created scheduled
+        let (importerId, (created, scheduled)) = func a
+         in addToUpcomingEventsMap today m importerId created scheduled
     )
 
-addToUpcomingEventsMap :: Day -> Map Day Word64 -> Day -> Day -> Map Day Word64
-addToUpcomingEventsMap today m created scheduled =
+addToUpcomingEventsMap ::
+  Day ->
+  Map Day (Map (Maybe ImporterMetadataId) Word64) ->
+  Maybe ImporterMetadataId ->
+  Day ->
+  Day ->
+  Map Day (Map (Maybe ImporterMetadataId) Word64)
+addToUpcomingEventsMap today m importerId created scheduled =
   let days = [created .. min today scheduled]
-      mapOfOnes = M.fromAscList $ map (\d -> (d, 1)) days
-   in M.unionWith (+) mapOfOnes m
+      mapOfOnes = M.fromAscList $ map (\d -> (d, M.singleton importerId 1)) days
+   in M.unionWith (M.unionWith (+)) mapOfOnes m
 
-makeUpcomingEventsMapWithLimit :: MonadUnliftIO m => Day -> Integer -> Acquire (ConduitT () a m ()) -> (a -> (Day, Day)) -> m (Map Day Word64)
+makeUpcomingEventsMapWithLimit ::
+  MonadUnliftIO m =>
+  Day ->
+  Integer ->
+  Acquire (ConduitT () a m ()) ->
+  (a -> (Maybe ImporterMetadataId, (Day, Day))) ->
+  m (Map Day (Map (Maybe ImporterMetadataId) Word64))
 makeUpcomingEventsMapWithLimit today limit source func =
   makeDayMap
     source
     ( \m a ->
-        let (created, scheduled) = func a
-         in addToUpcomingEventsMapWithLimit today limit m created scheduled
+        let (importerId, (created, scheduled)) = func a
+         in addToUpcomingEventsMapWithLimit today limit m created scheduled importerId
     )
 
-addToUpcomingEventsMapWithLimit :: Day -> Integer -> Map Day Word64 -> Day -> Day -> Map Day Word64
-addToUpcomingEventsMapWithLimit today limit m created scheduled =
+addToUpcomingEventsMapWithLimit ::
+  Day ->
+  Integer ->
+  Map Day (Map (Maybe ImporterMetadataId) Word64) ->
+  Day ->
+  Day ->
+  Maybe ImporterMetadataId ->
+  Map Day (Map (Maybe ImporterMetadataId) Word64)
+addToUpcomingEventsMapWithLimit today limit m created scheduled importerId =
   let days = [max created (addDays (-limit) scheduled) .. min today scheduled]
-      mapOfOnes = M.fromAscList $ map (\d -> (d, 1)) days
-   in M.unionWith (+) mapOfOnes m
+      mapOfOnes = M.fromAscList $ map (\d -> (d, M.singleton importerId 1)) days
+   in M.unionWith (M.unionWith (+)) mapOfOnes m
 
-makeDayMap :: MonadUnliftIO m => Acquire (ConduitT () a m ()) -> (Map Day Word64 -> a -> Map Day Word64) -> m (Map Day Word64)
+makeDayMap :: MonadUnliftIO m => Acquire (ConduitT () a m ()) -> (Map Day b -> a -> Map Day b) -> m (Map Day b)
 makeDayMap acqSource func = withAcquire acqSource $ \source ->
   runConduit $ source .| C.foldl func M.empty
+
+stackedLineChart :: Day -> Day -> Map Day (Map (Maybe ImporterMetadataId) Word64) -> Widget
+stackedLineChart minDay maxDay m =
+  let transposed :: Map (Maybe ImporterMetadataId) (Map Day Word64)
+      transposed = transposeMap m
+      named :: [(Text, Map Day Word64)]
+      named = map (first mImporterName) $ M.toList transposed
+   in lineChart minDay maxDay named
+
+mImporterName :: Maybe ImporterMetadataId -> Text
+mImporterName = \case
+  Nothing -> "Internal"
+  Just i -> T.pack $ show $ fromSqlKey i
+
+transposeMap :: (Ord a, Ord b) => Map a (Map b c) -> Map b (Map a c)
+transposeMap m =
+  fmap M.fromList . M.fromList $
+    [ (b, [(a, c) | (a, m'') <- M.toList m, (b', c) <- M.toList m'', b == b'])
+      | (_, m') <- M.toList m,
+        (b, _) <- M.toList m'
+    ]
 
 lineChart :: Day -> Day -> [(Text, Map Day Word64)] -> Widget
 lineChart minDay maxDay maps = do
