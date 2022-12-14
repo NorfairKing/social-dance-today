2f15694cc61e7ea7a5a00cde30b26bf40b44175e
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-12-14 11:35:20 +0100

    Run fewer unnecessary tests

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -4,7 +4,7 @@ with final.haskell.lib;
 
 {
 
-  salsaPartyReleasePackages = mapAttrs (_: pkg: justStaticExecutables (doCheck pkg)) final.haskellPackages.salsaPartyPackages;
+  salsaPartyReleasePackages = mapAttrs (_: pkg: justStaticExecutables pkg) final.haskellPackages.salsaPartyPackages;
 
   salsaPartyRelease = final.symlinkJoin {
     name = "salsa-party-release";
@@ -37,15 +37,9 @@ with final.haskell.lib;
                     doBenchmark = true;
                     doHaddock = false;
                     doCoverage = false;
-                    doCheck = false;
+                    doCheck = false; # Only on for the coverage report
                     enableLibraryProfiling = false;
                     enableExecutableProfiling = false;
-                    buildDepends = (old.buildInputs or [ ]) ++ (with final; [
-                      haskellPackages.autoexporter
-                    ]);
-                    preConfigure = (old.preConfigure or "") + ''
-                      export SALSA_PARTY_STATIC_DIR=${../static}
-                    '';
                     # Ugly hack because we can't just add flags to the 'test' invocation.
                     # Show test output as we go, instead of all at once afterwards.
                     testTarget = (old.testTarget or "") + " --show-details=direct";
@@ -77,24 +71,35 @@ with final.haskell.lib;
             {
               salsa-party-data = salsaPartyPkg "salsa-party-data";
               salsa-party-data-gen = salsaPartyPkg "salsa-party-data-gen";
-              salsa-party-web-server = withRemoteStaticResources (salsaPartyPkg "salsa-party-web-server") {
-                "static/bulma.css" = builtins.fetchurl {
-                  url = "https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css";
-                  sha256 = "0nbwcsa1gi36f2aq9y96bap7glkp40k3g2bjb9s1vmg0011sri1v";
-                };
-                "static/sentry.js" = builtins.fetchurl {
-                  url = "https://browser.sentry-cdn.com/7.12.1/bundle.tracing.min.js";
-                  sha256 = "sha256:08i3w728xdlzyvq4j9k1sm3ydfc6ch61cgs0wxvxqhc002vph5fa";
-                };
-                "static/instantpage.js" = builtins.fetchurl {
-                  url = "https://instant.page/5.1.1";
-                  sha256 = "sha256:0v5gmrd6wswnh92xqrmbk9gcf5yrmwl335j4fqyhdhz3dahyf3hd";
-                };
-              };
+              salsa-party-web-server = overrideCabal
+                (withRemoteStaticResources (salsaPartyPkg "salsa-party-web-server") {
+                  "static/bulma.css" = builtins.fetchurl {
+                    url = "https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css";
+                    sha256 = "0nbwcsa1gi36f2aq9y96bap7glkp40k3g2bjb9s1vmg0011sri1v";
+                  };
+                  "static/sentry.js" = builtins.fetchurl {
+                    url = "https://browser.sentry-cdn.com/7.12.1/bundle.tracing.min.js";
+                    sha256 = "sha256:08i3w728xdlzyvq4j9k1sm3ydfc6ch61cgs0wxvxqhc002vph5fa";
+                  };
+                  "static/instantpage.js" = builtins.fetchurl {
+                    url = "https://instant.page/5.1.1";
+                    sha256 = "sha256:0v5gmrd6wswnh92xqrmbk9gcf5yrmwl335j4fqyhdhz3dahyf3hd";
+                  };
+                })
+                (old: {
+                  preConfigure = (old.preConfigure or "") + ''
+                    export SALSA_PARTY_STATIC_DIR=${../static}
+                  '';
+                  buildDepends = (old.buildInputs or [ ]) ++ (with final; [
+                    haskellPackages.autoexporter
+                  ]);
+                });
               salsa-party-web-server-e2e = salsaPartyPkg "salsa-party-web-server-e2e";
               salsa-party-web-server-gen = salsaPartyPkg "salsa-party-web-server-gen";
               salsa-party-web-server-webdriver = final.haskellPackages.sydtest-webdriver.enableWebdriver (salsaPartyPkg "salsa-party-web-server-webdriver");
             };
+          # Until amazonka 2 is out and in nixpkgs:
+          # https://github.com/brendanhay/amazonka/issues/718
           amazonkaRepo = builtins.fetchGit {
             url = "https://github.com/brendanhay/amazonka";
             rev = "cfe2584aef0b03c86650372d362c74f237925d8c";
